fe6b6761b9022fb604935fd35f788e5fddda452d
santiocampo1 <sanntiocampo@gmail.com>
2024-08-11 11:41:49 -0300

    WebPage entity

--- server/google/src/main/java/com/googleclone/google/controllers/SearchController.java
+++ server/google/src/main/java/com/googleclone/google/controllers/SearchController.java
@@ -1,8 +1,12 @@
 package com.googleclone.google.controllers;
 
+import com.googleclone.google.entities.WebPage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class SearchController {
@@ -10,5 +14,9 @@ public class SearchController {
     @RequestMapping(value = "api/search", method = RequestMethod.GET)
     public List<WebPage> search() {
         List<WebPage> result = new ArrayList<>();
+        WebPage page = new WebPage();
+        page.setTitle("test");
+        page.setDescription("testing");
+        return result;
     }
 }
